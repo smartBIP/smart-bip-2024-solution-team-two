5d90c66bb71b1846aaf079870756c688e7453c6b
RaresTamaian <rarres_da@yahoo.com>
2024-02-14 15:12:26 +0100

    Temp and humidity sensor

--- sketch_feb14a.ino
+++ sketch_feb14a.ino
@@ -5,44 +5,81 @@
 TM1637Display display = TM1637Display(CLK, DIO);
 
 //temp
-// #include <OneWire.h>
-// #include <DallasTemperature.h>
 #define TEMP_SENSOR_PIN 5
 int tempVal;
-// sabotage sensor 8
-// led lamp is 6
-// ir sensor 2
+
+// temp + humidity
+#define DHT11_PIN 5
+#include <dht.h>
+dht DHT;
+
+// led lamp
+#define LED_LAMP 6
+
+// sabotage sensor
+#define SAB_SENSOR 8
+
+// ir count sensor for entrance sensor
+#define ENTRANCE_COUNT 2
 
 // sound 7
+#define BUZZ 7
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
   //display
   display.clear();
   display.setBrightness(7);
 
+  // buzzer
+  pinMode(BUZZ, OUTPUT); // Set buzzer - pin 9 as an output
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // Count sensor for entering
 
   // TEMP sensor
 
-  tempVal = analogRead(TEMP_SENSOR_PIN);  
-  // float mv = ( tempVal /1024.0)*5000;
-  float mv = (tempVal * 5.0 / 1023.0) * 100;
-  float cel = mv/10;
-  Serial.print("TEMPRATURE = ");
-  Serial.print(cel);
-  Serial.print("*C");
-  Serial.println();
+  // tempVal = analogRead(TEMP_SENSOR_PIN);  
+  // float mv = (tempVal * 5.0 / 1023.0) * 100;
+  // float cel = mv/10;
+  // Serial.print("TEMPRATURE = ");
+  // Serial.print(cel);
+  // Serial.print("*C");
+  // Serial.println();
 
+  //temp and humidity
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  // delay(1000);
+  
+  //buzz if the remperature is over 25
+  // if(cel > 25){
+  //   Serial.println("OVER");
+  //   tone(BUZZ, 1000); // Send 1KHz sound signal...
+  //   delay(1000);        // ...for 1 sec
+  //   noTone(BUZZ);     // Stop sound...
+  //   delay(1000);        // ...for 1sec
+  // }
+   if(DHT.temperature > 30){
+    Serial.println("OVER");
+    tone(BUZZ, 1000); // Send 1KHz sound signal...
+    delay(1000);        // ...for 1 sec
+    noTone(BUZZ);     // Stop sound...
+    delay(1000);        // ...for 1sec
+  }
   delay(1000);
 
-    // DISPLAY sensor
-      display.showNumberDec(cel);
+  // Count sensor for entering
+
+  // DISPLAY sensor
+  display.showNumberDec(DHT.temperature);
+
 
 
 }
